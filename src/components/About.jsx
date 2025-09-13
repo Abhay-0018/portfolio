7d79d3c7dfab13fbd19ec102e6a29088ea381060
Abhay-0018 <abhaykaiwart49@gmail.com>
2025-09-13 22:58:00 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/components/About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,7 +39,7 @@ const About = () => {
               </a>
       {/* Resume link  */}
               <a
-                href="https://drive.google.com/file/d/1b87fvsJ_IcsERKQnwh86LzqQsgnBLkBA/view"
+                href="https://drive.google.com/file/d/1b87fvsJ_IcsERKQnwh86LzqQsgnBLkBA/view" target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
